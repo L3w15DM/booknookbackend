00c5e943bf17732db6459874af9193cc0306adda
L3w15DM <117673970+L3w15DM@users.noreply.github.com>
2023-11-26 13:54:36 -0600

    created review and bookdetails controllers

---
+++ FullStackAuth_WebAPI/Controllers/BookDetailsController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.DataTransferObjects;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FullStackAuth_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BookDetailsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookDetailsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+      
+        [HttpGet("bookId")]
+        public IActionResult Get(string bookId)
+        {
+            
+            List<ReviewWithUserDto> reviews = _context.Reviews
+                .Where(b => b.BookId == bookId).Include(b => b.User).Select(r => new ReviewWithUserDto
+                {
+                    Id = r.Id,
+                    BookId = r.BookId,
+                    Text = r.Text,
+                    Rating = r.Rating,
+                    User = new UserForDisplayDto
+                    {
+                        Id = r.User.Id,
+                        FirstName = r.User.FirstName,
+                        UserName = r.User.UserName
+                    }
+                }).ToList();
+
+            string userId = User.FindFirstValue("id");
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized();
+            }
+
+            // sum of ratings
+            double ratingsSum = reviews.Select(r => r.Rating).Sum();
+
+            // average of ratings
+            double ratingsAvg = reviews.Count() > 0 ? ratingsSum / reviews.Count() : 0.0;
+            //
+            BookDetailsDto bookDetails = new BookDetailsDto
+            {
+                Reviews = reviews,
+                AverageRating = ratingsAvg
+            };
+
+
+            return StatusCode(200);
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
---
+++ FullStackAuth_WebAPI/Controllers/ReviewsController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using FullStackAuth_WebAPI.Data;
+using FullStackAuth_WebAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace FullStackAuth_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReviewsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ReviewsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost, Authorize]
+        public IActionResult Post([FromBody] Review review)
+        {
+            string userId = User.FindFirstValue("id");
+
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Unauthorized();
+            }
+            review.UserId = userId;
+
+            _context.Reviews.Add(review);
+            _context.SaveChanges();
+
+            return StatusCode(201, review);
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
+
